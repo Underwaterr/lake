fa5f5dff3d309ecec9e6a7d505eed052a546de4b
thoril-pegason <108502127+thoril-pegason@users.noreply.github.com>
2024-09-24 00:03:12 -0400

    rename 'check' endpoint to just 'login'

--- js/server/api/services/authentication/router.js
+++ js/server/api/services/authentication/router.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 
 let router = Router()
 
-router.get('/check', controller.check)
+router.get('/user', controller.check)
 router.post('/user', controller.loginUser)
 router.post('/decco', controller.loginDecco)
 router.get('/logout', controller.logout)
