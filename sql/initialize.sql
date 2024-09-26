c79d3c229326780de9b4187e351ee16fab883d50
thoril-pegason <108502127+thoril-pegason@users.noreply.github.com>
2024-09-26 04:19:31 -0400

    seed burn unit, survey and flight

--- sql/initialize.sql
+++ sql/initialize.sql
@@ -89,7 +89,7 @@ CREATE TABLE "BurnUnit" (
   "name"                text not null,
 
   "polygon"             geography(polygon),
-  "subpolygon"          geography(polygon),
+  "subpolygons"         geography(polygon),
 
   "organizationId"      int not null references "Organization"("id"),
   "createdById"         int not null references "User"("id")
@@ -182,3 +182,32 @@ INSERT INTO "Decco" (name, "isVirtual", callsign, "organizationId") VALUES
 
 INSERT INTO "DeccoAuthentication" ("deccoId", password) VALUES
 (1, '$argon2id$v=19$m=65536,t=3,p=4$w8UGwlBJcvyoOtLmh4eEeQ$pUDLDkGqXiG1PSfY4gGdk5BKMfhkTcO4YTZ32bhihYE');
+
+
+-- Need: burn unit, survey, flight
+INSERT INTO "BurnUnit" (name, "organizationId", "createdById", polygon, subpolygons)
+VALUES
+  ('first burn', 1, 1,
+    ST_GeomFromGeoJSON('{
+      "type":"Polygon",
+      "coordinates": [[
+        [ -71.110369302750115, 47.798430466372736 ],
+        [ -70.902563128560018, 47.983211774835986 ],
+        [ -70.699274479895777, 47.789325849015306 ],
+        [ -71.110369302750115, 47.798430466372736 ]
+      ]]
+    }'),
+    ST_GeomFromGeoJSON('{
+      "type":"Polygon",
+      "coordinates": [
+        [ [0, 0], [3, 3], [3, 0], [0, 0] ],
+        [ [4, 4], [14, 14], [14, 4], [4, 4] ]
+      ]
+    }')
+  );
+
+INSERT INTO "Survey" (type, "burnUnitId")
+VALUES ('PRE', 1), ('PERI', 1), ('POST', 1);
+
+INSERT INTO "Flight" ("deccoId", "pilotId", "surveyId")
+VALUES (1, 1, 1), (1, 1, 2), (1, 1, 3);
