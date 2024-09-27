3db9781844162b68480650c01add4e1a93e5d7d6
mcphail-alex <98819418+mcphail-alex@users.noreply.github.com>
2024-09-27 09:08:12 -0500

    changed Survey validator to match schema

--- js/server/api/entities/survey/validator.js
+++ js/server/api/entities/survey/validator.js
@@ -3,6 +3,6 @@ import vine from '@vinejs/vine'
 export default vine.compile(
   vine.object({
     type: vine.enum(['PRE', 'PERI', 'POST']),
-    burn_unit_id: vine.number()
+    burnUnitId: vine.number()
   })
 )
