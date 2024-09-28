e0286a00633fbb4a2852f9559aac81643ef7dfb9
thoril-pegason <108502127+thoril-pegason@users.noreply.github.com>
2024-09-28 17:54:22 -0400

    fix validation for flight events

--- js/server/api/entities/flight-event/validator.js
+++ js/server/api/entities/flight-event/validator.js
@@ -6,10 +6,10 @@ export default vine.compile(
     description: vine.string(),
     level: vine.enum(['LOW', 'MEDIUM', 'HIGH']),
     location: vine.any(),
-    start_time: vine.date({ formats: { utc: true } }),
+    startTime: vine.date({ formats: { utc: true } }),
     type: vine.enum(['TASK_STATUS', 'STATE_MACHINE', 'FLIGHT_CONTROL', 'FAILSAFE', 'INFO']).optional(),
 
-    decco_id: vine.number(),
-    flight_id: vine.number()
+    deccoId: vine.number(),
+    flightId: vine.number()
   })
 )
