b756a752f22c65532c427c8daa2beef657ba862b
thoril-pegason <108502127+thoril-pegason@users.noreply.github.com>
2024-10-09 13:00:24 -0400

    reduce now checks for non-awaited promises

--- js/server/api/entities/reduce.js
+++ js/server/api/entities/reduce.js
@@ -3,7 +3,8 @@
 // so `reduce` will confirm there is only one item as expected
 // and removes the superfluous array
 export default function(result) {
-  if(result.length==0) return ({ error: "No results" })
+  if(result instanceof Promise) return ({ error: "Promise not handled correctly" })
+  else if(result.length==0) return ({ error: "No results" })
   else if(result.length>1) throw new Error('Query for one row returned more than one row')
   else return result[0]
 }
