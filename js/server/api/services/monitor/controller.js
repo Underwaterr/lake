97439e453b37b8bd448d73813dd883e1fb762f3e
thoril-pegason <108502127+thoril-pegason@users.noreply.github.com>
2024-10-22 02:46:29 -0400

    database monitoring endpoint

--- js/server/api/services/monitor/controller.js
+++ js/server/api/services/monitor/controller.js
@@ -1,4 +1,7 @@
 import webSocketStore from '../../../web-socket/store.js'
+import { sql } from "squid/pg.js"
+import database from '../../../../database.js'
+import reduce from '../../entities/reduce.js'
 
 export default {
 
@@ -8,4 +11,13 @@ export default {
     console.log(store)
     response.json(store.get(organizationId))
   },
+
+  async database(request, response) {
+    // 90% or above is excellent
+    let result = reduce(await database.query(sql`
+      SELECT SUM(blks_hit) * 100 / SUM(blks_hit+blks_read) AS "hitRatio"
+      FROM pg_stat_database
+    ;`))
+    response.json(result)
+  }
 }
--- js/server/api/services/monitor/router.js
+++ js/server/api/services/monitor/router.js
@@ -4,5 +4,6 @@ import { Router } from 'express'
 let router = Router()
 
 router.get('/web-sockets', controller.webSockets)
+router.get('/database', controller.database)
 
 export default router
